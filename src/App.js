9eaff6cbd2ce0e72cc746496ed2fcf71db89cbb6
HaoLiHaiO <pinelwebdevelopment@gmail.com>
2021-08-20 12:12:01 +0300

    add links

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import Home from "./components/Home";
+import NasaPhoto from "./components/NasaPhoto";
+import MarsWeather from "./components/MarsWeather";
+import "./App.css";
+require("dotenv").config();
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="app">
+        <Route component={Home} path="/" exact />
+        <Route component={NasaPhoto} path="/nasaphoto" />
+        <Route component={MarsWeather} path="/marsweather" />
+      </div>
+    </BrowserRouter>
   );
 }
-
-export default App;
